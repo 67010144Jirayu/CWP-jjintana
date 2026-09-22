ea725d00a167fd87b28cf7c0e96affa62c2e551f
67010144-Jirayu <67010144@kmitl.ac.th>
2026-09-22 13:19:19 +0700

    Create play_with_arrays.py

---
+++ cell05/ex03/play_with_arrays.py
@@ -0,0 +1,12 @@
+x = [2, 8, 9, 48, 8, 22, -12, 2]
+y = []
+i = 0
+while i < len(x):
+    if x[i] > 5:
+        y.append(x[i]+2)
+        i += 1
+    else:
+        i += 1
+y = set(y)
+print(x)
+print(y)
