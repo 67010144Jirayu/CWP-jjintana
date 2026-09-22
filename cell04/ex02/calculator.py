0a40cd02d54840608014457b6f0067d2389e518f
67010144-Jirayu <67010144@kmitl.ac.th>
2026-09-22 12:46:02 +0700

    Create calculator.py

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,7 @@
+x = int(input("Give me the first number: "))
+y = int(input("Give me the second number: "))
+print("Thank you!")
+print(str(x) + " + " + str(y) + " = " + str(x + y))
+print(str(x) + " - " + str(y) + " = " + str(x - y))
+print(str(x) + " * " + str(y) + " = " + str(x * y))
+print(str(x) + " / " + str(y) + " = " + str(round(x / y)))
