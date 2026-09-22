da1e2760c998b5b722bf73f8a29e804ec39377c0
67010144-Jirayu <67010144@kmitl.ac.th>
2026-09-22 12:42:22 +0700

    Delete cell03/advanced_mult.py

--- cell03/advanced_mult.py
+++
@@ -1,9 +0,0 @@
-table = 0
-while table <= 10:
-    print("table de", table, ":", end=" ")
-    number = 0
-    while number <= 10:
-        print(table * number, end=" ")
-        number = number + 1
-    print()
-    table = table + 1
