77f0ddcaad6f2e66e1e32a6a2523a4b4877ff43c
67010144-Jirayu <67010144@kmitl.ac.th>
2026-09-24 16:40:56 +0700

    Add average function and print class averages

---
+++ cell07/ex02/help_your_professor.py
@@ -0,0 +1,22 @@
+def average(student):
+    total = 0
+    for name in student:
+        total = total + student[name]
+    return total / len(student)
+
+class_3B = {
+    "marine": 18,
+    "jean": 15,
+    "coline": 8,
+    "luc": 9
+}
+
+class_3C = {
+    "quentin": 17,
+    "julie": 15,
+    "marc": 8,
+    "stephanie": 13
+}
+
+print(f"Average for class 3B: {average(class_3B)}.")
+print(f"Average for class 3C: {average(class_3C)}.")
